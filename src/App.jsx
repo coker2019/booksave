a7007319fee765cb3b1147d761c12f242cc3d8ab
coker2019 <olabisi2k11@gmail.com>
2023-06-22 17:35:24 +0100

    Fixed Linters

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Categories" element={<Categories />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
 
     </div>
